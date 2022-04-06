0fb2ad0dfd6717e7a613adfbb021ebdcdc5d037b
Paweł Pyciński <pawel200020@gmail.com>
2022-04-06 23:54:54 +0200

    checkpoint2 with not working sliders

--- 2022_03_29/Traverser/js/script.js
+++ 2022_03_29/Traverser/js/script.js
@@ -1,6 +1,51 @@
-$(document).ready(function() {
+$(function() {
     $("#reader").load("js/read.php");
-    $(document).on('click', '#flip', function() {
-        $("#panel").slideDown("slow");
+    $('.content').hide();
+    var $elms = '.fruit, .vegetable, .dairy';
+
+    $(document).on('click', $elms, function() {
+        var $content = $(this).children('.content');
+        $content.stop(1, 1).slideToggle(600);
     });
+    var items = ['breakfast', 'lunch', 'dinner'];
+
+    $('.breakfast').on("hover", function(event) {
+        if (event.type == "mouseenter") {
+            console.log("entered");
+        } else if (event.type == "mouseleave") {
+            $(this).find('.dropfcnt').hide('blind', function() {
+                $('.actionfcnt').hide();
+            });
+        }
+    });
+
+    // $(items).each(function() {
+
+
+
+
+    //     var sel = this;
+
+
+
+    //     $(".top_level").on("hover", function(event) {
+    //         if (event.type == "mouseenter") {
+    //             $('.' + sel).parent().slideDown(400);
+    //             $('.' + sel).siblings(':not(.' + sel + ')').hide();
+    //         } else if (event.type == "mouseleave") {
+    //             $(this).find('.dropfcnt').hide('blind', function() {
+    //                 $('.actionfcnt').hide();
+    //             });
+    //         }
+    //     });
+
+    //     $('#' + sel).hover(function() {
+    //         $('.' + sel).parent().slideDown(400);
+    //         $('.' + sel).siblings(':not(.' + sel + ')').hide();
+    //     }, function() {
+    //         $('.' + sel).parent().slideUp(400);
+    //         //$('.' + sel).siblings(':not(.' + sel + ')').show();
+    //     });
+
+    // });
 });
